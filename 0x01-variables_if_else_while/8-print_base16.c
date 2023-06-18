f4d6e2bf3e44edb805c9c6933a0b384e87b0fb8a
Elinuel1 <adzimanuel@gmail.com>
2023-06-18 08:20:52 -0700

    8-base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print base sixteen
+ * Return: 0 (successfully executed)
+ */
+int main(void)
+{
+	char basesixteen = '0';
+
+	putchar(basesixteen);
+	putchar(basesixteen + 1);
+	putchar(basesixteen + 2);
+	putchar('\n');
+	return (0);
+}
