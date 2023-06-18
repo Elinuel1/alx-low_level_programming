c1cbac66dd20c698122de04388bfe130d915782c
Elinuel1 <adzimanuel@gmail.com>
2023-06-18 06:02:05 -0700

    3-alpHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints alphABETS
+ * Return: 0 (successfully executed)
+ */
+int main(void)
+{
+	char lowalpha = 'a';
+	char upalpha = 'A';
+
+	/* start of lowalpha */
+	while (lowalpha <= 'z')
+	{
+		putchar (lowalpha);
+		lowalpha++;
+	}
+
+	/* start of upalpha */
+	while (upalpha <= 'Z')
+	{
+		putchar (upalapha);
+		upalpha++;
+	}
+	putchar ('\');
+	return (0);
+}
