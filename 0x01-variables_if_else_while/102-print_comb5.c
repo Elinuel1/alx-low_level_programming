668932d3053ed085b98317c7ea8072203d27336f
Elinuel1 <adzimanuel@gmail.com>
2023-06-19 18:03:45 -0700

    102-print

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - combinations of two two-digit numbers
+ * Return: 0 (successfully executed)
+ */
+int main(void)
+{
+	int e, y;
+
+	for (e = 0; e < 100; e++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (e < y)
+			{
+				putchar((e/ 10) + 48);
+				putchar((e % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (i != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
