7673e8d2ca5c57cde25df632ada06df6eab5e76f
Elinuel1 <adzimanuel@gmail.com>
2023-06-23 18:11:09 +0100

    11-natural

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int print_to_98(int n);
+void print_to_98(int n);
 int print_times_table(int n);
 #endif
