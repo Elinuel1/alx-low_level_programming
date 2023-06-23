3433f270be94122f76f6c0c7ffd4a196b3d4c05d
Elinuel1 <adzimanuel@gmail.com>
2023-06-23 17:46:41 +0100

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addz teo integers and returns the result
+ * @p: int to be added to q
+ * @q: int to be added to p
+ * Return: sum of p and q
+ */
+int add(int p, int q)
+{
+	int z;
+
+	z = p + q;
+	return (z);
+}
